50358e35a4689915c43f9126838c161041375d3a
adriano-de-paula <adrianodepaula01@outlook.com>
2023-03-21 10:59:38 -0300

    Criou o Logo e adicionamos ele no App.js

---
+++ src/objects/Logo/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import GameLogo from '../../img/logo.png';
+import './styles.css';
+
+const Logo = () => (<img src={GameLogo} alt="Logo do jogo" className="logo" />);
+
+export default Logo;
